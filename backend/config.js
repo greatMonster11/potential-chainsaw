a45b9213595a327330eb136f5f96aa23f341bf13
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-03-03 11:39:03 +0700

    feat(server): successfully connect to MongoDB

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export default {
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27017/amazona"
+};
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,30 @@
 import express from "express";
 import data from "./data";
+import dotenv from "dotenv";
+import config from "./config";
+import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .catch(error => console.log(error.reason));
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("DB connected !!!");
+});
 
 const app = express();
 
+app.use("/api/users", userRoute);
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
